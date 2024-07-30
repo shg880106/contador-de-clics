a0f46521540feb92d34f75fab88548d8955603c2
Saily Hurtado <saily.hurtado@ntsprint.com>
2024-07-30 11:50:40 +0200

    Ultimando detalles de la aplicación Contador de Clics.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import './App.css';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import Contador from './componentes/Contador';
+import Boton from './componentes/Boton';
+import { useState } from 'react';
+
 
 function App() {
+
+  // Usando hocks: useState
+  const [numClics, setNumClics] = useState (0); 
+
+  // Definir una funcion flecha
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
   return (
     <div className="App">
-      
+      <div className='freecodecap-logo-contenedor'>
+        <img className='freecodecamp-logo' 
+        src={freeCodeCampLogo}
+        alt='Logo de freeCodeCamp' />
+      </div> 
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>     
     </div>
   );
 }
